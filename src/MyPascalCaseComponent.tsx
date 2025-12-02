b927c0c823212bd51dc83e0958dd591c30c61b7a
Tommy Lockwood <t.lockwood@erasolutions.us>
2025-12-02 10:00:17 -0600

    Initial commit

---
+++ src/MyPascalCaseComponent.tsx
@@ -0,0 +1,5 @@
+export default function MyPascalCaseComponent() {
+  return(
+    <p>Hello, World!</p>
+  );
+}
---
+++ src/main.tsx
@@ -0,0 +1,17 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.tsx'
+import MyPascalCaseComponent from './MyPascalCaseComponent.tsx';
+
+// const myCamelCaseVariable = 42;
+// const my_snake_case_variable = 42;
+// const MY_SCREAMING_SNAKE_CASE_VARIABLE = 42;
+// const MyPascalCaseVariable = 42;
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <MyPascalCaseComponent />
+    <App />
+  </StrictMode>,
+)
